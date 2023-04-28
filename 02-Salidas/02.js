7aae51db8c07ede75de5f3a43ea4338d56672f16
MrVile <mick.ortega.g@gmail.com>
2023-04-27 20:36:37 -0600

    JavaScript Outputs terminado

---
+++ 02-Salidas/02.js
@@ -0,0 +1,22 @@
+/**
+ * .innerHTML => Propiedad
+ * getElementById() => Metodo
+ */
+
+document.getElementById("textoPrueba").innerHTML = 5 + 5;
+document.write("Este texto es generado desde JS");
+
+/**
+ * Mostrar una alerta con window.alert()
+ */
+function alerta1() {
+  window.alert("Esta es una alerta escrita en JS y se muestra por el boton");
+}
+function alerta2() {
+  alert("Esta es una alerta escrita en JS y se muestra por el boton");
+}
+
+/**
+ * console.log
+ */
+console.log("Esta es la consola");
